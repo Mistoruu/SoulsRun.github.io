47cc1bf799d5d4d8fbfa042708b0071ea3e7aa2f
Mistoruu <150136429+Mistoruu@users.noreply.github.com>
2024-01-18 12:28:58 +0100

    Add files via upload

---
+++ WEB-TEST/WEB-TEST/Core/Interactions/script.js
@@ -0,0 +1,46 @@
+const demonssouls_game = {
+    name: 'Demons Souls',
+    year: 2009,
+    runner: 'inSilico_',
+
+}
+const ds1_game = {
+    name: 'Dark Souls ',
+    year: '2011',
+    runner: 'QueueKyoo',
+
+}
+const ds2_game ={
+    name:'Dark Souls 2',
+    year: 2014,
+    runner: 'ruhanzito',
+
+}
+
+const ds3_game ={
+    name: 'Dark souls 3',
+    year: 2016,
+    runner:'olzku23',
+
+}
+
+const bloodborne_game={
+    name:'Bloodborne',
+    year: 2015,
+    runner:'Genso',
+
+}
+
+const sekiro_game={
+    name:'Sekiro',
+    year: 2019,
+    runner: 'Sev1nG',
+
+}
+
+const eldenring_game={
+    name: 'Elden Ring',
+    year: 2022,
+    runner:'Forsa',
+
+}
